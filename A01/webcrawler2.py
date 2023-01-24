e9e2441d8767757c5f8b07a3fdcacf10b15c29dd
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-01-24 01:36:53 -0500

    Update google scholar crawler

--- A01/webcrawler2.py
+++ A01/webcrawler2.py
@@ -1,6 +1,7 @@
 import sys
 import requests
 import hashlib
+import json
 from bs4 import BeautifulSoup
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
@@ -16,12 +17,13 @@ def get_page(url):
      try:
           response = requests.get(url, headers=HEADERS)
           if response.status_code == 200:
-               return response.text
+               return response
           else:
+               print(response.status_code)
                return None
      except:
           return None
-
+          
 def get_content(url):
      """
      Description:
@@ -32,10 +34,24 @@ def get_content(url):
      """
      page = get_page(url)
      if page:
-          soup = BeautifulSoup(page, 'html.parser')
-          return soup.text
+          soup = BeautifulSoup(page.content, 'html.parser')
+          return soup.prettify()
 
-def write_to_file(content, url):
+def get_parsed_content(url):
+     """
+     Description:
+          Returns the parsed content of the page at the given URL.
+          
+     Parameters:
+          url (str): The URL of the page to retrieve.
+     """
+     soup = get_content(url)
+     researcher_name = soup.find('div', id='gsc_prf_in')
+     print(researcher_name)
+
+     return researcher_name
+
+def write_raw_data(content, url):
      """
      Description:
           Writes the content to a file with hashed name.
@@ -44,9 +60,20 @@ def write_to_file(content, url):
           content (str): The content to write to the file.
           url (str): The URL of the page to retrieve.
      """
-     filename = hash_url(url) + '.txt'
+     filename = 'data/' + hash_url(url) + '.txt'
      with open(filename, 'w') as f:
           f.write(content)
+
+def write_json_data():
+     """
+     Description:
+          Writes the content to a file with hashed name.
+     
+     Parameters:
+          content (str): The content to write to the file.
+          url (str): The URL of the page to retrieve.
+     """
+     pass
 
 def hash_url(url):
      """
@@ -73,7 +100,8 @@ def main():
 
      content = get_content(url)
      if content:
-          write_to_file(content, url)
+          write_raw_data(content, url)
+          write_json_data(content, url)
      else:
           print("Error. Unable to retrieve this flaming heap of garbage.")
 
