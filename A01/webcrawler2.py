12ed638afdf448a8d24201121963073d4f0516d5
Aidan Traboulay <aidantraboulay13@gmail.com>
2023-01-25 07:16:23 -0500

    Add unnecessary lines of code

--- A01/webcrawler2.py
+++ A01/webcrawler2.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import hashlib
 import json
+from time import sleep 
 from bs4 import BeautifulSoup
 
 HEADERS = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
@@ -151,6 +152,29 @@ def hash_url(url):
      """
      return hashlib.sha256(url.encode()).hexdigest()
           
+def loading(iter, total, prefix='', suffix='', decimals=1, length=100, fill='>'):
+     """
+     Description:
+          A loading bar for the command line. Yes, I have time on my hands.
+
+     Parameters:
+          iter (int): The current iteration.
+          total (int): The total number of iterations.
+          prefix (str): The prefix to display before the loading bar.
+          suffix (str): The suffix to display after the loading bar.
+          decimals (int): The number of decimals to display.
+          length (int): The length of the loading bar.
+          fill (str): The character to use to fill the loading bar.
+     """
+     percent = ("{0:." + str(decimals) + "f}").format(100 * (iter / float(total)))
+     filled_len = int(length * iter // total)
+     bar = fill * filled_len + '-' * (length - filled_len)
+     
+     print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = '\r')
+     
+     if iter == total:
+          print()
+
 def main():
      """
      Description:
@@ -164,8 +188,29 @@ def main():
      except:
           print("Error. No URL argument provided.")
 
+     items = list(range(0, 50))
+     l = len(items)
+          
+
+     print(r"""
+
+                                   ._ o o
+                                   \_`-)|_
+                                ,""       \ 
+                              ,"  ## |   ಠ ಠ. 
+                            ," ##   ,-\__    `.
+                          ,"       /     `--._;)
+                        ,"     ## /
+                      ,"   ##    /
+     """)
+
+     loading(0, l, prefix='Progress:', suffix='Complete', length=l)
+     for i, item in enumerate(items):
+          sleep(0.09)
+          loading(i + 1, l, prefix='Progress:', suffix='Complete', length=l)
+
      content = get_content(url).prettify()
-     
+
      if content:
           write_raw_data(content, url)
           write_json_data(content, url)
