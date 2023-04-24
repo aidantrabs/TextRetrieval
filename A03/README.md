ff2a44dcb4593b368102dd647cb5c4ce6ad93f9c
Nausher Rao <nausher.rao@shopify.com>
2023-04-24 11:17:10 -0400

    Finish training_sentiment.py

--- A03/README.md
+++ A03/README.md
@@ -27,30 +27,37 @@
 
 ## Contributions
  #### **Aidan Traboulay** 200115590
- - 
+ -
 
 ####  **Mobina Tooranisama** 200296720
-- 
+-
 
 ####  **Nausher Rao** 190906250
-- 
+- I worked on everything inside the `training_sentiment.py` file, including all functions and definitions.
 
 
 ## Explanations
 
 
 ### Training Sentiment (`training_sentiment.py`)
-- 
-
+- The program first parses the arguments provided by the user. If no dataset argument and/or classifier argument is provided, the program will let the user know to select one of each. If more than one dataset and/or classifier argument is provided, the program will run the first one from the list. This is done by the `arg_handler` and `parse_args` functions.
+- The program then loads the dataset into a pandas dataframe from the `data` folder. This is done by the `load_dataset` function.
+- The program then loads the classifier into a vectorizer by the `init_vectorizer` function.
+- The data is parsed by the `process_text` function, and used to train the classifier in the `init_classifier` function. The classifier is then used to predict the sentiment of the test data.
+- The metrics of the classifier is then calculated and printed to the console using the `calculate_performance_metrics` and `calculate_cross_validation_performance_metrics` functions.
+- Lastly, the program generates and saves a confusion matrix to the disk, as well as serializing the classifier and vectorizer to the disk. This is done by the `save_confusion_matrix` and `serialize_classifier_and_vectorizer` functions.
 #### Usage
 ```sh
-python3 training_sentiment.py
+python3 training_sentiment.py [-h] ([--imdb] [--amazon] [--yelp]) ([--naive] [--svm] [--decisiontree] [--knn <n>])
 ```
+- The program requires one of the following dataset arguments: `--imdb`, `--amazon`, `--yelp`. If more than one dataset is provided, the program will run the first one from the list.
+
+- The program also requires one of the following classifier arguments: `--naive`, `--svm`, `--decisiontree`, `--knn <n>`. If more than one classifier is provided, the program will run the first one from the list.
 
 ### Cluster News (`cluster_news.py`)
-- 
+-
 
 #### Usage
 ```sh
-python3 cluster_news.py 
+python3 cluster_news.py
 ```
--- A03/training_sentiment.py
+++ A03/training_sentiment.py
@@ -1,40 +1,34 @@
-import argparse;
-import csv
+import argparse
 import enum
 import joblib
 import matplotlib.pyplot as plt
-import nltk;
-import nltk.corpus;
-import nltk.stem;
-import nltk.tokenize;
-import numpy;
-import pandas;
+import nltk
+import nltk.corpus
+import nltk.stem
+import nltk.tokenize
+import numpy
+import pandas
 import seaborn as sns
 import sklearn
+import sklearn.metrics
+import sklearn.naive_bayes
+import sklearn.neighbors
+import sklearn.svm
+import sklearn.tree
 
-from sklearn.feature_extraction.text import CountVectorizer;
-from typing import List, Union;
-
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
-from sklearn.model_selection import KFold
-from sklearn.naive_bayes import MultinomialNB
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.svm import SVC
-from sklearn.tree import DecisionTreeClassifier
-
+from sklearn.model_selection import KFold, cross_val_score
+from typing import Union
 
 CONFUSION_MATRIX_FILE_NAME = "confusion_matrix.png"
+VECTORIZER_OUTPUT_FILE_NAME = "vectorizer.joblib"
+CLASSIFIER_OUTPUT_FILE_NAME = "classifier.joblib"
 
 class ClassifierType(enum.Enum):
     NAIVE_BAYES = 1,
     KNN = 2,
     SVM = 3,
-    DECISION_TREE = 4;
+    DECISION_TREE = 4
 
 
 def arg_handler():
@@ -42,11 +36,8 @@ def arg_handler():
     Description:
         Handle the arguments using argparse.
 
-    Parameters:
-        None
-
     Returns:
-        args: the arguments.
+        args: the arguments in a map structure.
     """
     parser = argparse.ArgumentParser()
     dataset = parser.add_argument_group("Dataset")
@@ -65,10 +56,10 @@ def arg_handler():
 def parse_args(args: object):
     """
     Description:
-        Parse the arguments.
+        Parse the arguments collected by argparse and retrieve the dataset and classifier to use.
 
     Parameters:
-        args: The arguments.
+        args: The arguments returned by arg_handler.
 
     Returns:
         dataset: The dataset to use.
@@ -138,10 +129,10 @@ def init_classifier(dataset_file_name: str, classifierType: ClassifierType, n: U
         n: The n value to use if KNN is chosen as the classifier.
 
     Returns:
-        vectorizer: The vectorizer.
-        classifier: The classifier.
-        X: The X.
-        Y: The Y.
+        vectorizer: The count vectorizer.
+        classifier: The classifier fitted with the X and Y vector.
+        X: The X vector.
+        Y: The Y vector.
     """
     file_name = f"data/training_sentiment/{dataset_file_name}"
     data = pandas.read_csv(file_name, sep='\t', header=None)
@@ -152,41 +143,40 @@ def init_classifier(dataset_file_name: str, classifierType: ClassifierType, n: U
     Y = data[1]
 
     if(classifierType == ClassifierType.NAIVE_BAYES):
-        classifier = MultinomialNB()
+        classifier = sklearn.naive_bayes.MultinomialNB()
 
     elif(classifierType == ClassifierType.KNN):
-        classifier = SVC()
+        classifier = sklearn.svm.SVC()
 
     elif(classifierType == ClassifierType.SVM):
-        classifier = DecisionTreeClassifier()
+        classifier = sklearn.tree.DecisionTreeClassifier()
 
     elif(classifierType == ClassifierType.DECISION_TREE):
-        classifier = KNeighborsClassifier(n)
+        classifier = sklearn.neighbors.KNeighborsClassifier(n)
 
     classifier.fit(X, Y)
     return vectorizer, classifier, X, Y
 
 
-def calculate_performance_metrics(X, Y, y_pred):
+def calculate_performance_metrics(Y, y_pred):
     """
     Description:
-        Calculate the performance metrics.
+        Calculate the performance metrics using the given vectors.
 
     Parameters:
-        classifier: The classifier.
-        X: The X.
-        Y: The Y.
+        Y: The Y vector.
+        y_pred: The predicted Y vector.
 
     Returns:
-        accuracy: The accuracy.
-        precision: The precision.
-        recall: The recall.
-        f1: The f1 score.
+        accuracy: The accuracy of the classifier.
+        precision: The precision of the classifier.
+        recall: The recall of the classifier.
+        f1: The f1 score of the classifier.
     """
-    accuracy = accuracy_score(Y, y_pred)
-    precision = precision_score(Y, y_pred, average="macro")
-    recall = recall_score(Y, y_pred, average="macro")
-    f1 = f1_score(Y, y_pred, average="macro")
+    accuracy = sklearn.metrics.accuracy_score(Y, y_pred)
+    precision = sklearn.metrics.precision_score(Y, y_pred, average="macro")
+    recall = sklearn.metrics.recall_score(Y, y_pred, average="macro")
+    f1 = sklearn.metrics.f1_score(Y, y_pred, average="macro")
 
     return accuracy, precision, recall, f1
 
@@ -194,12 +184,12 @@ def calculate_performance_metrics(X, Y, y_pred):
 def calculate_cross_validation_performance_metrics(classifier, X, Y, kf):
     """
     Description:
-        Calculate the cross validation metrics.
+        Calculate the cross validation performance metrics of a given classifier.
 
     Parameters:
         classifier: The classifier.
-        X: The X.
-        Y: The Y.
+        X: The X vector.
+        Y: The Y vector.
         kf: The KFold object.
 
     Returns:
@@ -208,47 +198,72 @@ def calculate_cross_validation_performance_metrics(classifier, X, Y, kf):
         recall: The recall of the classifier.
         f1: The f1 score of the classifier.
     """
-    from sklearn.model_selection import cross_val_score
-    accuracy = cross_val_score(classifier, X, Y, cv=kf, scoring="accuracy")
-    precision = cross_val_score(classifier, X, Y, cv=kf, scoring="precision_macro")
-    recall = cross_val_score(classifier, X, Y, cv=kf, scoring="recall_macro")
-    f1 = cross_val_score(classifier, X, Y, cv=kf, scoring="f1_macro")
+    accuracy = numpy.mean(cross_val_score(classifier, X, Y, cv=kf, scoring="accuracy"))
+    precision = numpy.mean(cross_val_score(classifier, X, Y, cv=kf, scoring="precision_macro"))
+    recall = numpy.mean(cross_val_score(classifier, X, Y, cv=kf, scoring="recall_macro"))
+    f1 = numpy.mean(cross_val_score(classifier, X, Y, cv=kf, scoring="f1_macro"))
 
     return accuracy, precision, recall, f1
 
 
-def save_confusion_matrix(X, Y, y_pred):
+def print_metrics(title: str, dataset: str, classifierType: ClassifierType, accuracy: float, precision: float, recall: float, f1: float):
     """
     Description:
-        Plot the confusion matrix.
+        Print the performance metrics.
 
     Parameters:
-        classifier: The classifier.
-        X: The X.
-        Y: The Y.
+        title: The title of the print.
+        dataset: The dataset used.
+        classifierType: The classifier type used.
+        accuracy: The accuracy of the classifier.
+        precision: The precision of the classifier.
+        recall: The recall of the classifier.
+        f1: The f1 score of the classifier.
     """
-    matrix = confusion_matrix(Y, y_pred)
-    sns.heatmap(matrix, annot=True, fmt="d")
+    print()
+    print(f"{title} | {dataset} using {classifierType.name}:")
+    print("==============================================")
+    print(f"Accuracy: {accuracy:.5f}")
+    print(f"Precision: {precision:.5f}")
+    print(f"Recall: {recall:.5f}")
+    print(f"F1: {f1:.5f}")
+    print("==============================================")
+    print()
+    return
+
+
+def save_confusion_matrix(Y, y_pred):
+    """
+    Description:
+        Save the confusion matrix to the disk.
+
+    Parameters:
+        Y: The Y vector.
+        y_pred: The predicted Y vector.
+    """
+    matrix = sklearn.metrics.confusion_matrix(Y, y_pred)
+
+    sns.heatmap(matrix, fmt="d", annot=True)
+    plt.title("Confusion Matrix")
     plt.xlabel("Predicted Label")
     plt.ylabel("Actual Label")
-    plt.title("Confusion Matrix")
     plt.savefig(CONFUSION_MATRIX_FILE_NAME)
     return
 
 
-def save_vectorizer_and_classifier(vectorizer, classifier):
+def serialise_vectorizer_and_classifier(vectorizer, classifier):
     """
     Description:
-        Save the vectorizer and classifier to the disk.
+        Save and dump the vectorizer and classifier to the disk.
 
     Parameters:
         vectorizer: The vectorizer.
         classifier: The classifier.
     """
-    with open("vectorizer.dat", "wb") as file:
+    with open(VECTORIZER_OUTPUT_FILE_NAME, "wb") as file:
         joblib.dump(vectorizer, file)
 
-    with open("classifier.dat", "wb") as file:
+    with open(CLASSIFIER_OUTPUT_FILE_NAME, "wb") as file:
         joblib.dump(classifier, file)
 
     return
@@ -269,22 +284,12 @@ def main():
     kf = KFold(n_splits=5, shuffle=True, random_state=42)
     y_pred = classifier.predict(X)
 
-    # Calculate performance metrics
-    accuracy, recall, precision, f1 = calculate_performance_metrics(X, Y, y_pred)
-    print(f"Performance Metrics - {dataset} - {classifierType}:")
-    print(f"")
-
-    print("Performance of Classification:\n\tAccuracy: {:.3f}\n\tRecall: {:.3f}\n\tPrecision: {:.3f}\n\tF1-score: {:.3f}".format(accuracy, recall, precision, f1))
-
-    # Calculate performance metrics with cross-validation
-    cross_accuracy, cross_recall, cross_precision, cross_f1 = calculate_cross_validation_performance_metrics(classifier, X, Y, kf)
-    print("Evaluation of Performance with Cross-Validation:\n\tAccuracy: {:.3f}\n\tRecall: {:.3f}\n\tPrecision: {:.3f}\n\tF1-score: {:.3f}".format(np.mean(cross_accuracy), np.mean(cross_recall), np.mean(cross_precision), np.mean(cross_f1)))
-
-    # Plot the confusion matrix
-    save_confusion_matrix(X, Y, y_pred)
-
-    # Save the vectorizer and classifier
-    save_vectorizer_and_classifier(vectorizer, classifier)
+    accuracy, recall, precision, f1 = calculate_performance_metrics(Y, y_pred)
+    accuracy_c, recall_c, precision_c, f1_c = calculate_cross_validation_performance_metrics(classifier, X, Y, kf)
+    print_metrics("Performance Metrics", dataset, classifierType, accuracy, precision, recall, f1)
+    print_metrics("Cross Validation Performance Metrics", dataset, classifierType, accuracy_c, precision_c, recall_c, f1_c)
+    save_confusion_matrix(Y, y_pred)
+    serialise_vectorizer_and_classifier(vectorizer, classifier)
     return
 
 
